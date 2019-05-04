004a79c30040d9b38d3767d9fd1845da900e955f
BohdanBuhrii <bobuhrii@gmail.com>
2019-05-04 13:48:16 -0700

    problem with interface

--- Models/User.cs
+++ Models/User.cs
@@ -9,6 +9,23 @@ namespace Models
 {
     class User : IInformable
     {
+        public long user_id;
+        public string user_name;
+        public string email;
+        public string date_of_birth;
+
+        public string[] GetFieldsName()
+        {
+            return new string[] {
+                "user_id", "user_name", "email", "date_of_birth"};
+        }
+
+        public string[] GetFieldsValue() //проблема як визначати тип поля при записі в базу даних
+        {
+            return new string[] {
+                user_id.ToString(), user_name, email, hash_password,
+                is_employee.ToString(), access_level};
+        }
     }
 }
 

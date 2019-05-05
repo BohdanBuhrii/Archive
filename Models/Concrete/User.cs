a51ab9f727ff5105eaeaf79ef177f084b2e12fe8
BohdanBuhrii <bobuhrii@gmail.com>
2019-05-04 19:57:15 -0700

    namespace fixed

--- Models/Concrete/User.cs
+++ Models/Concrete/User.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
 using Models.Abstract;
 
-namespace Models
+namespace Models.Concrete
 {
     public class User : IModel
     {

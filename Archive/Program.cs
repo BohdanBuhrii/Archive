0f04833ae9f94b4b141298cd24c8a5154ea4657e
BohdanBuhrii <bobuhrii@gmail.com>
2019-05-05 18:00:30 -0700

    Database.IssuanceFormsRepo ready

--- Archive/Program.cs
+++ Archive/Program.cs
@@ -13,6 +13,7 @@ namespace Archive
         {
             //DocumentsRepo repo = new DocumentsRepo();
             //UsersRepo userRepo = new UsersRepo();
+            IssuanceFormsRepo issuanceFormsRepo = new IssuanceFormsRepo();
 
             //repo.Add(new Document { document_id=2, existence=true, owner_id=1, type="заліковка" });
             //if(repo.Delete(new DocumentFilter { owner_id = 1, type = "заліковка"})==false) Console.Write("error");
@@ -22,7 +23,11 @@ namespace Archive
             //userRepo.Add(new User { user_id = 6, user_name = "Olesya", email = "olesya@gmail.com", date_of_birth = new DateTime(2000,2,19) });
             //userRepo.Update(new UserFilter { user_name="Petro" },new UserFilter { user_name="Vasya", email="vasya@gmail.com" });
             //var b=userRepo.Get(new UserFilter { user_id=1 });
-
+            //issuanceFormsRepo.Add(new IssuanceForm { date_of_issue = new DateTime(2019, 5, 5), document_id = 1, user_id = 1, was_returned = false });
+            //issuanceFormsRepo.Update(
+            //    new IssuanceFormFilter { date_of_issue = new DateTime(2019, 5, 5) }, new IssuanceFormFilter { was_returned = true });
+            //var c=issuanceFormsRepo.Get(new IssuanceFormFilter { document_id=1, user_id=1 });
+            
             //Console.Read();
         }
     }
--- Repository/Concrete/Database/IssuanceFormsRepo.cs
+++ Repository/Concrete/Database/IssuanceFormsRepo.cs
@@ -1,33 +1,188 @@
 ﻿using Models.Abstract;
+using Models.Concrete;
+using Models.Concrete.Filters;
 using Repository.Abstract;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Common;
 
 namespace Repository.Concrete.Database
 {
-    class IssuanceFormsRepo : ConnectionManager, IRepository
+    public class IssuanceFormsRepo : ConnectionManager, IRepository
     {
         public bool Add(IModel model)
         {
+            try
+            {
+                IssuanceForm issuanceForm = (IssuanceForm)model;
+                ExecuteNonQuery(string.Format(
+                    "INSERT INTO issuanceforms (date_of_issue, document_id, user_id, was_returned) " +
+                    "VALUES ('{0}',{1},{2},{3})",
+                    issuanceForm.date_of_issue, issuanceForm.document_id, issuanceForm.user_id, issuanceForm.was_returned));
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+
             return true;
         }
 
         public bool Delete(IModelFilter filter)
         {
+            try
+            {
+                IssuanceFormFilter issuanceFormFilter = (IssuanceFormFilter)filter;
+                string query = "DELETE FROM issuanceforms WHERE ";
+                bool optionAdded = false;
+
+                if (issuanceFormFilter.date_of_issue.HasValue)
+                {
+                    query += "date_of_issue='" + issuanceFormFilter.date_of_issue + "'";
+                    optionAdded = true;
+                }
+                if (issuanceFormFilter.document_id.HasValue)
+                {
+                    if (optionAdded) query += " and ";
+                    query += "document_id=" + issuanceFormFilter.document_id;
+                    optionAdded = true;
+                }
+                if (issuanceFormFilter.user_id.HasValue)
+                {
+                    if (optionAdded) query += " and ";
+                    query += "user_id=" + issuanceFormFilter.user_id;
+                    optionAdded = true;
+                }
+                if (issuanceFormFilter.was_returned.HasValue)
+                {
+                    if (optionAdded) query += " and ";
+                    query += "was_returned=" + issuanceFormFilter.was_returned;
+                }
+
+                ExecuteNonQuery(query);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
             return true;
         }
 
         public bool Update(IModelFilter filter, IModelFilter model)
         {
+            try
+            {
+                IssuanceFormFilter issuanceFormFilter = (IssuanceFormFilter)filter;
+                IssuanceFormFilter issuanceForm = (IssuanceFormFilter)model;
+                string query = "UPDATE issuanceforms SET ";
+                bool optionAdded = false;
+
+                if (issuanceForm.date_of_issue.HasValue)
+                {
+                    query += "date_of_issue='" + issuanceForm.date_of_issue + "'";
+                    optionAdded = true;
+                }
+                if (issuanceForm.document_id.HasValue)
+                {
+                    if (optionAdded) query += ", ";
+                    query += "document_id=" + issuanceForm.document_id;
+                    optionAdded = true;
+                }
+                if (issuanceForm.user_id.HasValue)
+                {
+                    if (optionAdded) query += ", ";
+                    query += "user_id=" + issuanceForm.user_id;
+                    optionAdded = true;
+                }
+                if (issuanceForm.was_returned.HasValue)
+                {
+                    if (optionAdded) query += ", ";
+                    query += "was_returned=" + issuanceForm.was_returned;
+                }
+
+                query += " WHERE ";
+                optionAdded = false;
+
+                if (issuanceFormFilter.date_of_issue.HasValue)
+                {
+                    query += "date_of_issue='" + issuanceFormFilter.date_of_issue + "'";
+                    optionAdded = true;
+                }
+                if (issuanceFormFilter.document_id.HasValue)
+                {
+                    if (optionAdded) query += " and ";
+                    query += "document_id=" + issuanceFormFilter.document_id;
+                    optionAdded = true;
+                }
+                if (issuanceFormFilter.user_id.HasValue)
+                {
+                    if (optionAdded) query += " and ";
+                    query += "user_id=" + issuanceFormFilter.user_id;
+                    optionAdded = true;
+                }
+                if (issuanceFormFilter.was_returned.HasValue)
+                {
+                    if (optionAdded) query += " and ";
+                    query += "was_returned=" + issuanceFormFilter.was_returned;
+                }
+
+                ExecuteNonQuery(query);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
             return true;
         }
 
         public List<IModel> Get(IModelFilter filter)
         {
             List<IModel> result = new List<IModel>();
+
+            IssuanceFormFilter issuanceFormFilter = (IssuanceFormFilter)filter;
+            string query = "SELECT * FROM issuanceforms WHERE ";
+            bool optionAdded = false;
+
+            if (issuanceFormFilter.date_of_issue.HasValue)
+            {
+                query += "date_of_issue='" + issuanceFormFilter.date_of_issue + "'";
+                optionAdded = true;
+            }
+            if (issuanceFormFilter.document_id.HasValue)
+            {
+                if (optionAdded) query += " and ";
+                query += "document_id=" + issuanceFormFilter.document_id;
+                optionAdded = true;
+            }
+            if (issuanceFormFilter.user_id.HasValue)
+            {
+                if (optionAdded) query += " and ";
+                query += "user_id=" + issuanceFormFilter.user_id;
+                optionAdded = true;
+            }
+            if (issuanceFormFilter.was_returned.HasValue)
+            {
+                if (optionAdded) query += " and ";
+                query += "was_returned=" + issuanceFormFilter.was_returned;
+            }
+
+            DbDataReader reader = ExecuteReader(query);
+
+            while (reader.Read())
+            {
+                result.Add(new IssuanceForm
+                {
+                    date_of_issue = (DateTime)reader["date_of_issue"],
+                    document_id = (long)reader["document_id"],
+                    user_id = (long)reader["user_id"],
+                    was_returned = (bool)reader["was_returned"]
+                });
+            }
+
+            RefreshDataReader();
 
             return result;
         }

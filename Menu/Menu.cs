623972105a19d9386a1b21f6646a4ca1d59c4796
BohdanBuhrii <bobuhrii@gmail.com>
2019-05-06 11:05:31 -0700

    Menu ready

--- Menu/Menu.cs
+++ Menu/Menu.cs
@@ -21,7 +21,7 @@ namespace Menu
             int choise=0;
             while (choise != 5)
             {
-                Console.Write("See information (1)\n" +
+                Console.Write("\nSee information (1)\n" +
                               "Take the document (2)\n" +
                               "Take back the document (3)\n" +
                               "See debtors (4)\n" +
@@ -57,7 +57,7 @@ namespace Menu
                     User user=(User)usersRepo.Get(new UserFilter { user_name = Console.ReadLine() })[0];
 
                     //try
-                    List<IModel> documents = documentsRepo.Get(new DocumentFilter { owner_id=user.user_id });
+                    List<IModel> documents = documentsRepo.Get(new DocumentFilter { owner_id=user.user_id, existence=true });
 
                     Console.WriteLine("Choose document:");
 
@@ -79,7 +79,7 @@ namespace Menu
                     })
                     )
                     {
-                        documentsRepo.Update(new DocumentFilter)
+                        documentsRepo.Update(new DocumentFilter { owner_id= user.user_id, document_id= ((Document)documents[documentNumber]).document_id }, new DocumentFilter { existence = false });
                         Console.WriteLine("\nDone");
                     }
                     else Console.WriteLine("\nError");
@@ -87,11 +87,48 @@ namespace Menu
                 }
                 else if (choise == 3)
                 {
+                    //todo
+                    Console.WriteLine("Enter user's name: ");
 
+                    //try
+                    User user = (User)usersRepo.Get(new UserFilter { user_name = Console.ReadLine() })[0];
+
+                    List<IModel> issuanceForms = issuanceformsRepo.Get(new IssuanceFormFilter { user_id=user.user_id });
+
+                    Console.WriteLine("Choose issuance form:");
+
+                    for (int i = 0; i < issuanceForms.Count; i++)
+                    {
+                        Console.WriteLine((i + 1).ToString() + ":\n" + issuanceForms[i]);
+                    }
+
+                    int issuanceFormNumber = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                    if (
+                    issuanceformsRepo.Update(new IssuanceFormFilter
+                    {
+                        user_id = user.user_id,
+                        document_id = ((IssuanceForm)issuanceForms[issuanceFormNumber]).document_id
+                    },
+                        new IssuanceFormFilter { was_returned = true })
+
+                    )
+                    {
+                        documentsRepo.Update(new DocumentFilter { owner_id = user.user_id, document_id = ((IssuanceForm)issuanceForms[issuanceFormNumber]).document_id }, new DocumentFilter { existence = true });
+                        Console.WriteLine("\nDone");
+                    }
+                    else Console.WriteLine("\nError");
                 }
                 else if (choise == 4)
                 {
-
+                    List<IModel> issuanceforms= issuanceformsRepo.Get(new IssuanceFormFilter { was_returned=false });
+                    if (issuanceforms.Count > 0)
+                    { foreach (IModel model in issuanceforms)
+                        {
+                            Console.WriteLine(usersRepo.Get(new UserFilter { user_id = ((IssuanceForm)model).user_id}));
+                        }
+                    }
+                    else Console.WriteLine("No debtors");
                 }
             }
         }
